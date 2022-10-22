c673a43e86ced39823dcb460ac6b703a6a6def88
Emily <emily2004.yao@gmail.com>
2022-10-22 02:45:12 +0000

    adding files so far

---
+++ UDPserver.py
@@ -0,0 +1,23 @@
+import socket
+
+HOST = "10.140.41.16"
+PORT = 20
+
+print("establishing server on %s:%s" % (HOST,PORT))
+with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as UDP_server_socket:
+    UDP_server_socket.bind((HOST,PORT))
+    print("server running on %s:%s" % (HOST,PORT))
+
+    while True:
+        incoming = UDP_server_socket.recvfrom(1024)
+        message = incoming[0]
+        address = incoming[1]
+
+        f = open("img.jpg", "rb")
+        l = f.read(1024)
+        while l:
+            UDP_server_socket.sendto(l, address)
+            l = f.read(1024)
+        f.close()
+
+        print("img sent")
---
+++ client.py
@@ -0,0 +1,36 @@
+import socket
+import time
+
+HOST = "10.141.164.25"
+PORT = 20
+
+# Creating client socket
+print("trying to connect to %s:%s..." % (HOST,PORT))
+try:
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client_socket.connect((HOST,PORT))
+    print("connected to port %s:%s" % (HOST,PORT))
+    #print("-----BEGIN-----")
+except socket.error as e:
+    print("could not connect: ", e)
+    exit()
+
+# # Receiving incoming file (only used if running server.py simultanously)
+# start_time = time.time()
+# f = open("received.png", "wb")
+# l = client_socket.recv(1024)
+# while l:
+#     f.write(l)
+#     l = client_socket.recv(1024)
+
+start_time = time.time()
+f = open("img.jpg", "rb")
+l = f.read(1024)
+while l:
+    client_socket.send(l)
+    l = f.read(1024)
+
+f.close()
+client_socket.close()
+#print("-----END-----")
+print("img sent")
